3fa71e0c231e03c58ea977d83b70da34f3b3fc11
rkreddy253 <rkr253@gmail.com>
2024-07-30 23:09:30 -0400

    Revert "modified second python file"
    This reverts commit 71dd9feeb300356f281bf800b06a0dfefe4395f9.

--- second-git.py
+++ second-git.py
@@ -1,7 +1,8 @@
 print("this is my second file for git commit")
 name = input()
 if name == "rama":
-	print("this is rama")
-else:
-	print("this is someone else")
+  print("this is rama")
+elif:
+  print("this is someone else")
+
 print("Done")
